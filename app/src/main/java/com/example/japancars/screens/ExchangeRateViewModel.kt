ff4226e7c42a6ba5ed02173e4e8aa83879d6d966
Naint <dolmatovcom@inbox.ru>
2024-02-04 21:10:23 +0300

    Реализация модели для парсинга курсов

--- app/src/main/java/com/example/japancars/screens/ExchangeRateViewModel.kt
+++ app/src/main/java/com/example/japancars/screens/ExchangeRateViewModel.kt
@@ -14,6 +14,16 @@ class ExchangeRateViewModel(application: Application): AndroidViewModel(applicat
     private var yenRate: Double = 0.0
     private var euroRate: Double = 0.0
 
+    private var yenRateSwift: Double = 0.0
+    private var dollarRateSwift: Double = 0.0
+
+    public var euroRateCb: Double = 0.0
+    private var dollarRateCb: Double = 0.0
+    private var yenRateCb: Double = 0.0
+    private var vonRateCb: Double = 0.0
+
+
+
     fun getRateInfo(){
         try{
             viewModelScope.launch(Dispatchers.IO) {
@@ -27,6 +37,32 @@ class ExchangeRateViewModel(application: Application): AndroidViewModel(applicat
         }
         catch (_ : Exception){
         }
+    }
+
+    fun getRatesCb(){
+        try{
+            viewModelScope.launch(Dispatchers.IO){
+                val doc = Jsoup.connect("https://cbr.ru/currency_base/daily/").get()
+                val rateTable = doc.getElementsByTag("table")[0]
+                dollarRateCb = rateTable
+                    .getElementsByTag("tr")[14]
+                    .getElementsByTag("td")[4].text().replace(",", ".").toDouble()
+                euroRateCb = rateTable
+                    .getElementsByTag("tr")[15]
+                    .getElementsByTag("td")[4].text().replace(",", ".").toDouble()
+                yenRateCb = rateTable
+                    .getElementsByTag("tr")[40]
+                    .getElementsByTag("td")[4].text().replace(",", ".").toDouble()
+                vonRateCb = rateTable
+                    .getElementsByTag("tr")[8]
+                    .getElementsByTag("td")[4].text().replace(",", ".").toDouble()
+
+            }
+
+        }catch (_ : Exception){
+
+        }
+        Log.i("rates", "$dollarRateCb $euroRateCb $yenRateCb $vonRateCb")
     }
 
     private fun setEuroRate(eR: Double){
--- app/src/main/java/com/example/japancars/screens/other/OtherFragment.kt
+++ app/src/main/java/com/example/japancars/screens/other/OtherFragment.kt
@@ -6,21 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.japancars.R
+import com.example.japancars.databinding.FragmentOtherBinding
+import com.example.japancars.screens.exchangeRate.ExchangeRatesFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [OtherFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class OtherFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private var _binding: FragmentOtherBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +34,24 @@ class OtherFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_other, container, false)
+        _binding = FragmentOtherBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.cvCurrencyRate.setOnClickListener{
+            replaceFragment(ExchangeRatesFragment())
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = parentFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.main_layout, fragment)
+        fragmentTransaction.commit()
     }
 
     companion object {
